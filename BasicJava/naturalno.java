c4a54967252a6e539c4ba20d4cc3e45b3c987a3a
maddheshiyaabhay <maddeshiyaabhay856@gmail.com>
2024-07-24 13:23:33 +0530

    :fghsb

---
+++ BasicJava/naturalno.java
@@ -0,0 +1,28 @@
+package BasicJava;
+
+public class naturalno {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=1; i<= 10; i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print("even"+i);
+             continue;
+            }System.out.println("odd"+i);
+        }
+
+
+//        int i=10;
+//        while(i>=1) {
+//            System.out.println(i);
+//            i--;
+//    }
+//        int i=10;
+//        do{
+//            System.out.println(i);
+//            i--;
+//        }while(i>=1);
+
+    }
+}
